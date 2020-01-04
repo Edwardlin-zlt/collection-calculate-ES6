1d47f986e3a30c4ffe88251da374e2e9ad7a1281
Edwardlin-zlt <edwardlin.zlt@gmail.com>
2020-01-04 16:17:13 +0800

    flaten done!

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
 
 function double_to_one(collection) {
-
   //在这里写入代码
+  var result = [];
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i] instanceof Array) {
+      for (var ii = 0; ii < collection[i].length; ii++) {
+        result.push(collection[i][ii]);
+      }
+    } else {
+      result.push(collection[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = double_to_one;
--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,21 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var result = [];
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i] instanceof Array) {
+      for (var ii = 0; ii < collection[i].length; ii++) {
+        if (!result.includes(collection[i][ii])){
+          result.push(collection[i][ii]);
+        }
+      }
+    } else {
+      if (!result.includes(collection[i])){
+        result.push(collection[i]);
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = double_to_one;
