b8184d58b3647c264699250585e86675d38a7e53
Edwardlin-zlt <edwardlin.zlt@gmail.com>
2020-01-05 16:10:43 +0800

    section3 done!

--- main/section_3/practice_2/practice.js
+++ main/section_3/practice_2/practice.js
@@ -1,5 +1,14 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  var toCompareValue = object_b.value;
+
+  collection_a.forEach(ele => {
+    if (toCompareValue.includes(ele.key) && ele.count >=3){
+      ele.count -= Math.floor(ele.count/3);
+    }
+  })
+
+  return collection_a;
 }
 
 module.exports = create_updated_collection;
--- main/section_3/practice_4/practice.js
+++ main/section_3/practice_4/practice.js
@@ -1,5 +1,53 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  var toCompareValue = object_b.value;
+  
+  var countedArray = countChar(collection_a);
+
+  countedArray.forEach(ele => {
+    if (toCompareValue.includes(ele.key) && ele.count >=3){
+      ele.count -= Math.floor(ele.count/3);
+    }
+  })
+
+  return countedArray;
+}
+
+function countChar(collection){
+ var countDic = {};
+  collection.forEach(ele => {
+    switch (true){
+      case ele.length === 1:
+        if (!countDic[ele]){
+          countDic[ele] = 1;
+        } else {
+          countDic[ele] += 1;
+        }
+        break;
+      case ele.length > 1:
+        var keyAndValue = ele.match(/^(\w).*?(\d+).*/);
+        var key = keyAndValue[1];
+        var value = parseInt(keyAndValue[2]);
+        if (!countDic[key]){
+          countDic[key] = value;
+        } else {
+          countDic[key] += value;
+        }
+        break;
+    }
+  })
+  return dicToFormatedArray(countDic);
+}
+
+function dicToFormatedArray(dic){
+  var result = []
+  for (key in dic){
+    var item = {};
+    item.key = key;
+    item.count = dic[key];
+    result.push(item);
+  }
+  return result;
 }
 
 module.exports = create_updated_collection;
