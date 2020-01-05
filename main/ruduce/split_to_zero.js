f2eb25ce9eb875b64b7662abfba5de29d2a6079e
Edwardlin-zlt <edwardlin.zlt@gmail.com>
2020-01-05 12:07:49 +0800

    ruduce done!

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -2,6 +2,14 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  var result = [number];
+  while (number > 0){
+    number = number*10;
+    number -= interval*10;
+    number = number /10;
+    result.push(number);
+  }
+  return result;
 }
 
 module.exports = spilt_to_zero;
